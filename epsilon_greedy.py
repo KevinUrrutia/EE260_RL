2db7efbabef2c365898bbc0d75aaef19fd7299ae
Kevin Urrutia <kevinurrutia@pop-os.localdomain>
2023-05-02 00:10:56 -0700

    finished

--- epsilon_greedy.py
+++ epsilon_greedy.py
@@ -2,8 +2,7 @@ import numpy as np
 import random as rnd
 
 def selectAction(Q, s, epsilon):
-    p_a = rnd.random();
-    if(p_a < epsilon):
+    if(rnd.uniform(0, 1) < epsilon):
         A = rnd.randint(0, 1)
     else:
         A = np.argmax(Q[s])
@@ -28,32 +27,34 @@ p_s_a1 = np.array([[0.9, 0.1, 0, 0, 0],
                    [0.9, 0, 0.1, 0, 0],
                    [0.9, 0, 0.1, 0, 0]])
 
-Q = np.random.rand(5, 2)
+Q = np.zeros((5, 2))
 
 gamma = 0.95
-theta = 0.001
+theta = 1e-5
 delta = 0
-alpha = 0.01
+alpha = 0.1
 epsilon = 0.1
+s = 0
 
-returns = []
 
 while(True):
     delta = 0
-    for s in range(np.size(Q, 1)):
-        A_star = selectAction(Q, s, epsilon) #select A using an epsilon greedy policy
-        q = Q[s][A_star]
-        R = r[A_star][s]
-        if(A_star == 0):
-            S_prime = np.argmax(p_s_a0[s])
-        else:
-            S_prime = np.argmax(p_s_a1[s])
+    A_star = selectAction(Q, s, epsilon) #select A using an epsilon greedy policy
+    q = Q[s][A_star]
+    R = r[s][A_star]
+    if(A_star == 0):
+        S_prime = np.argmax(p_s_a0[s])
+    else:
+        S_prime = np.argmax(p_s_a1[s])
 
-        Q[s][A_star] = Q[s][A_star] + alpha*(R + gamma*max(Q[s]) - Q[s][A_star])
 
-        delta = max(delta, abs(q - Q[s][A_star]))
-        print(delta)
-    if(delta < theta):
-        break
+    Q[s][A_star] = Q[s][A_star] + alpha*(R + gamma*max(Q[S_prime]) - Q[s][A_star])
 
-print(Q)
+    delta = max(delta, abs(q - Q[s][A_star]))
+    # print(abs(q - Q[s][A_star]))
+
+    s = S_prime
+    # if(delta < theta):
+    #     break
+
+    print(Q)
