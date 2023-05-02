dfb3ba39f4a35b058bd90792145390faa8d0a307
Kevin Urrutia <kevinurrutia@pop-os.localdomain>
2023-05-02 12:48:22 -0700

    updated

--- dynamic_program.py
+++ dynamic_program.py
@@ -30,15 +30,19 @@ while(True):
     delta = 0
     for s in range(np.size(V, 1)):
         next_states = np.nonzero(p_s_a0[s])
+        sum_ao = 0
         for s_prime in next_states[0]:
-            Q[s][0] = p_s_a0[s_prime][s] + (r[s][0] + gamma*max(Q[s_prime]))
+            sum_ao = p_s_a0[s][s_prime] + (r[s][0] + gamma*max(Q[s_prime]))
 
+        sum_a1 = 0
         next_states = np.nonzero(p_s_a1[s])
         for s_prime in next_states[0]:
-            Q[s][1] = p_s_a1[s_prime][s] + (r[s][1] + gamma*max(Q[s_prime]))
+            sum_a1 = p_s_a1[s][s_prime] + (r[s][1] + gamma*max(Q[s_prime]))
 
+        Q[s][0] = sum_ao
+        Q[s][1] = sum_a1
         print(Q/2)
-        delta = max(delta, abs(q - Q[s][A_star]))
+        # delta = max(delta, abs(q - Q[s][A_star]))
 
     # if(delta < theta):
     #     break
